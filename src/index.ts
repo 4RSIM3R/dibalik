34318b396fd8699956a55e4a5f8c84d7cd97defc
ilzam <ilzammulkhaq85@gmail.com>
2023-12-25 22:02:45 +0700

    feat: simple version

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,68 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+export interface Environment {
 
-export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+}
+
+const corsHeaders = {
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+	"Access-Control-Max-Age": "86400",
+};
+function handleOptions(request: Request) {
+	const headers = request.headers;
+	if (
+		headers.get("Origin") !== null &&
+		headers.get("Access-Control-Request-Method") !== null &&
+		headers.get("Access-Control-Request-Headers") !== null
+	) {
+		const respHeaders = {
+			...corsHeaders,
+			"Access-Control-Allow-Headers": request.headers.get("Access-Control-Request-Headers") as string
+		};
+		return new Response(null, { headers: respHeaders });
+	} else {
+		return new Response(null, { headers: { Allow: "GET, HEAD, POST, OPTIONS" } });
+	}
+}
+
+async function handleRequest(request: Request) {
+	const origin = request.headers.get("Origin") as string;
+	const url = new URL(request.url);
+	const target = url.searchParams.get("url");
+
+	console.log(url);
+
+	if (target == null) { return Teapot(); }
+
+	request = new Request(target, request);
+	request.headers.set("Origin", new URL(target).origin);
+
+	let response = await fetch(request);
+	response = new Response(response.body, response);
+	response.headers.set("Access-Control-Allow-Origin", origin);
+	response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+	response.headers.append("Vary", "Origin");
+
+	return response;
+}
+
+/**
+ * 
+ * @returns 
+ */
+function Teapot() {
+	return new Response(null, {
+		status: 418,
+		statusText: "I'm a teapot"
+	});
 }
 
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+	async fetch(request: Request): Promise<Response> {
+
+		if (request.method === "OPTIONS") {
+			return handleOptions(request);
+		} else {
+			return handleRequest(request);
+		}
+	}
 };
